f56a77a7a43993ae8954815e641ba55f51bd84e5
SharkDeveloper <48812383+SharkDeveloper@users.noreply.github.com>
2022-02-20 05:31:49 +0300

    Delta_Height
    Изменение высоты
    
    Co-Authored-By: SharkDeveloper <48812383+SharkDeveloper@users.noreply.github.com>

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -473,8 +473,8 @@
  * details can be tuned in Configuration_adv.h
  */
 
-#define THERMAL_PROTECTION_HOTENDS // Enable thermal protection for all extruders
-#define THERMAL_PROTECTION_BED     // Enable thermal protection for the heated bed
+//#define THERMAL_PROTECTION_HOTENDS // Enable thermal protection for all extruders
+//#define THERMAL_PROTECTION_BED     // Enable thermal protection for the heated bed
 
 //===========================================================================
 //============================= Mechanical Settings =========================
@@ -539,12 +539,12 @@
   #define DELTA_DIAGONAL_ROD 220 // mm
 
   // height from z=0 to home position
-  #define DELTA_HEIGHT 285 // get this value from auto calibrate
+  #define DELTA_HEIGHT 283 // get this value from auto calibrate
 
   #define DELTA_ENDSTOP_ADJ { 0.0, 0.0, 0.0 } // get these from auto calibrate
 
   // Horizontal distance bridged by diagonal push rods when effector is centered.
-  #define DELTA_RADIUS 103 //mm  Get this value from auto calibrate
+  #define DELTA_RADIUS 136 //mm  Get this value from auto calibrate
 
   // Trim adjustments for individual towers
   // tower angle corrections for X and Y tower / rotate XYZ so Z tower angle = 0
