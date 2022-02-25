ccb905944aa318c8ae0af628e4025ea599ce578b
SharkDeveloper <48812383+SharkDeveloper@users.noreply.github.com>
2022-02-25 08:01:35 +0300

    DELTA_ROD DELTA_RADIUS v2.0
    Подгон высоты, уменьшение линзы
    
    Co-Authored-By: SharkDeveloper <48812383+SharkDeveloper@users.noreply.github.com>

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -539,7 +539,7 @@
   #define DELTA_DIAGONAL_ROD 250 // mm
 
   // height from z=0 to home position
-  #define DELTA_HEIGHT 28.7 // get this value from auto calibrate
+  #define DELTA_HEIGHT 280.7 // get this value from auto calibrate
 
   #define DELTA_ENDSTOP_ADJ { 0.0, 0.0, 0.0 } // get these from auto calibrate
 
